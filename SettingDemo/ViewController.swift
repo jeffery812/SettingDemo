dfaf3e7239917b67f940c2f6d43e6a2f7664a9e4
Zhihui Tang <crafttang@gmail.com>
2015-12-10 11:36:02 +0100

    added

--- SettingDemo/ViewController.swift
+++ SettingDemo/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openSettings(sender: AnyObject) {
+        let url:NSURL! = NSURL(string : "prefs:root=")
+        UIApplication.sharedApplication().openURL(url)
+    }
 
 }
 
