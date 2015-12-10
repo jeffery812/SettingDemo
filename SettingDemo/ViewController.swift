3ab3978e4ee3bbece5f5195dc733a5c7ad23e57e
Zhihui Tang <crafttang@gmail.com>
2015-12-10 11:27:36 +0100

    Initial Commit

---
+++ SettingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettingDemo
+//
+//  Created by Zhihui Tang on 10/12/15.
+//  Copyright © 2015 Zhihui Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
